d06c7ef680f054f6ed8d2964a1850c98dcb778c9
Florencia <mflorenciacarranza@gmail.com>
2022-06-22 20:07:35 -0300

    Clase Contenedor desafío 2 y métodos varios

--- Desafío2/carrito.js
+++ Desafío2/carrito.js
@@ -8,5 +8,48 @@
 //deleteAll(): void – Elimina todos los objetos presentes en el archivo
 
 
-const productos = fs.readFileSync('./productos.txt', 'utf-8')
-console. log(productos)
+const  Contenedor  =  require ( './app.js' )
+
+let  contenedor =  new  Contenedor ( 'productos.txt' ) ;
+
+ carrito  const =  asincrono () => {
+    consola _ log ( '------leemos el archivo por si tenemos productos de antes' )
+    consola _ log ( aguardar  contenedor . getAll ( ) ) ;  
+
+    consola _ log ( '------agregamos productos' )
+    
+    const  obj1 =  { título : 'Crema Nutritiva con Liposomas' , precio : 1200 , thumbnail: 'https://ceyc.com.ar/wp-content/uploads/EXEL_862-Gel-Cream-con-Ceramidas-50-ml.jpg' }
+    consola _ log ( aguardar  contenedor . guardar ( obj1 ) ) ;
+    const  obj2 =  { título : 'Gel Cream con Ceramidas' ,  precio : 1500 , thumbnail: 'https://ceyc.com.ar/wp-content/uploads/EXEL_862-Gel-Cream-con-Ceramidas-50-ml.jpg' }          
+    consola _ log ( aguardar  contenedor . guardar ( obj2 ) ) ;
+    const  obj3 =  { título : 'Shampoo Exel Argan y Maracuya' ,  precio : 1100 ,  thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_884455-MLA31692481145_082019-O.jpg' }
+    
+    consola _ log ( aguardar  contenedor . guardar ( obj3 ) ) ;
+    consola _ registro ( '---------' )
+    consola _ log ( '----Contenedor despues' )
+    consola _ log ( aguardar  contenedor . getAll ( ) ) ; 
+    
+    consola _ registro ( '---------' )
+    consola _ log ( '--getById(2)' ) 
+    consola _ log ( espera  contenedor . getById ( 2 ) )
+    consola _ log ( '--getById(34) : debe devolver null' )
+    consola _ log ( espera  contenedor . getById ( 34 ) )
+    
+    consola _ registro ( '---------' )
+    consola _ log ( '----Contenedor Antes' )
+    consola _ log ( aguardar  contenedor . getAll ( ) ) ; 
+    consola _ registro ( '------' )
+    consola _ registro ( '--deleteById(2)' )
+    consola _ log ( espera  contenedor . deleteById ( 2 ) )
+    consola _ log ( '----Contenedor despues' )
+    consola _ log ( espera  contenedor . getAll ( ) )
+    
+    consola _ registro ( '---------' )
+    consola _ registro ( '--deleteAll' )
+    consola _ log ( espera  contenedor . deleteAll ( ) ) 
+    consola _ registro ( '---------' )
+    consola _ registro ( '--getAll' )
+    consola _ log ( await  contenedor . getAll ( ) )
+}
+
+carrito () ;
