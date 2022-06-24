5ecc6261af20591793980c546e002ba1c4d36ed5
Florencia <mflorenciacarranza@gmail.com>
2022-06-24 20:30:14 -0300

    Corrijo app.js, carrito.js, y productos.txt

--- Desafío2/app.js
+++ Desafío2/app.js
@@ -1,79 +1,67 @@
-const  fs  =  requerir ( 'fs' )
+const fs = require('fs');
 
-clase   Contenedor  {
-    constructor  ( tiendaExel ) {
-        esto _ tiendaExel =   tiendaExel ;
+class Contenedor {
+  constructor(nombreArchivo) {
+    this.nombreArchivo = nombreArchivo;
+  }
+
+  getAll = async () => {
+    try {
+      const contenido = await fs.promises.readFile(this.nombreArchivo, 'utf-8');
+      return JSON.parse(contenido);
+    } catch (error) {
+      console.error(error);
+      await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([], null, 2));
+      const contenido = await fs.promises.readFile(this.nombreArchivo, 'utf-8');
+      return JSON.parse(contenido);
     }
+  }
 
-    getAll =  async ( ) => {
-        carrito {
-            const  contenido  =  esperar  fs . promesas _ readFile ( este . tiendaExel ,  'utf-8' )
-            devolver  JSON . analizar ( contenido )
-        } atrapar ( error ) {
-            consola _ log ( 'error de lectura' )
-            esperar  fs . promesas _ writeFile ( this . tiendaExel , JSON . stringify ( [ ] ,  null ,  2 ) )
-            const  contenido  =  esperar  fs . promesas _ readFile ( este . nombreArchivo ,  'utf-8' )
-            devolver  JSON . analizar ( contenido )
+  save = async (objeto) => {
+    try {
+      const arrObjetos = await this.getAll();
+      arrObjetos.push({ ...objeto, id: arrObjetos.length + 1 });
+      await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(arrObjetos, null, 2));
+      return objeto.identificación;
+    } catch (error) {
+      console.error(error);
+      throw new Error('No se pudo guardar');
+    }
+  }
+
+  getById = async (id) => {
+    try {
+      const arrObjetos = await this.getAll();
+      return arrObjetos.find(obj => (obj.id === id)) || null;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error al obtener el objeto por Id');
+    }
+  }
+
+  deleteById = async (id) => {
+    try {
+      const arrObjetos = await this.getAll();
+      for (let i = 0; i < arrObjetos.length; i++) {
+        if (id === arrObjetos[i].id) {
+          arrObjetos.splice(i, 1);
         }
+      }
+      await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(arrObjetos, null, 2))
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error al borrar el objeto por Id');
     }
+  }
 
-    save  =  async ( objeto ) => {  
-        const  arrObjetos =  esperar  esto . obtenerTodo ( ) ;
-        sea  ​​maxId  =  0 ;
-        arrObjetos . paraCada ( valor  =>  {
-                if  ( valor . id  >  maxId )  {
-                    maxId  =  valor . identificación ; }
-            }
-        ) ;
-        idmax += 1 ;  
-
-        const  obj =  ( { título : objeto . título ,  precio : objeto . precio ,  miniatura : objeto . miniatura ,  id : maxId } )
-        arrObjetos . empujar ( objetivo )
-        carrito {
-            esperar  fs . promesas _ writeFile ( this . tiendaExel ,  JSON . stringify ( arrObjetos ,  null ,  2 ) )
-            devolver  objeto . identificación ;
-        } atrapar {
-            lanzar  nuevo  Error ( 'No se pudo guardar' ) ;
-        }
+  deleteAll = async () => {
+    try {
+      await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([], null, 2))
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error al borrar todos los objetos');
     }
-
-    getById =  async ( id ) =>  { 
-
-        carrito {
-            const  arrObjetos =  esperar  esto . obtenerTodo ( ) ;
-            let  salida =   arrObjetos . encontrar (  obj =>  ( obj . id === id ) )
-            si  ( salida ) {
-                volver  salida
-            } más {
-                return  null 
-            }
-        } atrapar {
-            throw  new  Error ( 'Error al obtener el Id' ) ;
-        }
-    }
-
-    deleteById =  async ( id ) => {
-        carrito {
-            const  arrObjetos =  esperar  esto . obtenerTodo ( ) ;
-            for  ( sea  i = 0 ;  i <  arrObjetos . length ; i ++ )
-            {   
-                if ( id === arrObjetos [ i ] . id ) {
-                    arrObjetos . splice ( i , 1 )  
-                } 
-            }
-            esperar  fs . promesas _ writeFile ( this . tiendaExel ,  JSON . stringify ( arrObjetos ,  null ,  2 ) )
-        } atrapar {
-            throw  new  Error ( 'Error al obtener al borrar el Id' ) ;
-        }
-    }
-
-    deleteAll =  async ( ) => { 
-        carrito {
-            esperar  fs . promesas _ writeFile ( this . tiendaExel , JSON . stringify ( [ ] ,  null ,  2 ) )
-        }  atrapar ( error ) {
-            throw  new  Error ( 'Error al obtener al borrar todos los objetos' ) ;
-        }
-    }
+  }
 }
 
-module _ exportaciones =  Contenedor ;
+module.exports = Contenedor;
--- Desafío2/carrito.js
+++ Desafío2/carrito.js
@@ -8,48 +8,48 @@
 //deleteAll(): void – Elimina todos los objetos presentes en el archivo
 
 
-const  Contenedor  =  require ( './app.js' )
+const Contenedor = require('./app.js')
 
-let  contenedor =  new  Contenedor ( 'productos.txt' ) ;
+let contenedor = new Contenedor('productos.txt');
 
- carrito  const =  asincrono () => {
-    consola _ log ( '------leer el archivo por si tenemos productos de antes' )
-    consola _ log ( aguardar  contenedor . getAll ( ) ) ;  
+const carrito = async () => {
+  console.log('------leer el archivo por si tenemos productos de antes')
+  console.log(await contenedor.getAll());
 
-    consola _ log ( '------agregar productos' )
-    
-    const  obj1 =  { título : 'Crema Nutritiva con Liposomas' , precio : 1200 , thumbnail: 'https://ceyc.com.ar/wp-content/uploads/EXEL_862-Gel-Cream-con-Ceramidas-50-ml.jpg' }
-    consola _ log ( aguardar  contenedor . guardar ( obj1 ) ) ;
-    const  obj2 =  { título : 'Gel Cream con Ceramidas' ,  precio : 1500 , thumbnail: 'https://ceyc.com.ar/wp-content/uploads/EXEL_862-Gel-Cream-con-Ceramidas-50-ml.jpg' }          
-    consola _ log ( aguardar  contenedor . guardar ( obj2 ) ) ;
-    const  obj3 =  { título : 'Shampoo Exel Argan y Maracuya' ,  precio : 1100 ,  thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_884455-MLA31692481145_082019-O.jpg' }
-    
-    consola _ log ( aguardar  contenedor . guardar ( obj3 ) ) ;
-    consola _ registro ( '---------' )
-    consola _ log ( '----Contenedor despues' )
-    consola _ log ( aguardar  contenedor . getAll ( ) ) ; 
-    
-    consola _ registro ( '---------' )
-    consola _ log ( '--getById(2)' ) 
-    consola _ log ( espera  contenedor . getById ( 2 ) )
-    consola _ log ( '--getById(34) : debe devolver null' )
-    consola _ log ( espera  contenedor . getById ( 34 ) )
-    
-    consola _ registro ( '---------' )
-    consola _ log ( '----Contenedor Antes' )
-    consola _ log ( aguardar  contenedor . getAll ( ) ) ; 
-    consola _ registro ( '------' )
-    consola _ registro ( '--deleteById(2)' )
-    consola _ log ( espera  contenedor . deleteById ( 2 ) )
-    consola _ log ( '----Contenedor despues' )
-    consola _ log ( espera  contenedor . getAll ( ) )
-    
-    consola _ registro ( '---------' )
-    consola _ registro ( '--deleteAll' )
-    consola _ log ( espera  contenedor . deleteAll ( ) ) 
-    consola _ registro ( '---------' )
-    consola _ registro ( '--getAll' )
-    consola _ log ( await  contenedor . getAll ( ) )
+  console.log('------agregar productos')
+
+  const obj1 = { título: 'Crema Nutritiva con Liposomas', precio: 1200, thumbnail: 'https://ceyc.com.ar/wp-content/uploads/EXEL_862-Gel-Cream-con-Ceramidas-50-ml.jpg' }
+  console.log(await contenedor.save(obj1));
+  const obj2 = { título: 'Gel Cream con Ceramidas', precio: 1500, thumbnail: 'https://ceyc.com.ar/wp-content/uploads/EXEL_862-Gel-Cream-con-Ceramidas-50-ml.jpg' }
+  console.log(await contenedor.save(obj2));
+  const obj3 = { título: 'Shampoo Exel Argan y Maracuya', precio: 1100, thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_884455-MLA31692481145_082019-O.jpg' }
+
+  console.log(await contenedor.save(obj3));
+  console.log('---------')
+  console.log('----Contenedor despues')
+  console.log(await contenedor.getAll());
+
+  console.log('---------')
+  console.log('--getById(2)')
+  console.log(await contenedor.getById(2))
+  console.log('--getById(34) : debe devolver null')
+  console.log(await contenedor.getById(34))
+
+  console.log('---------')
+  console.log('----Contenedor Antes')
+  console.log(await contenedor.getAll());
+  console.log('------')
+  console.log('--deleteById(2)')
+  console.log(await contenedor.deleteById(2))
+  console.log('----Contenedor despues')
+  console.log(await contenedor.getAll())
+
+  console.log('---------')
+  console.log('--deleteAll')
+  console.log(await contenedor.deleteAll())
+  console.log('---------')
+  console.log('--getAll')
+  console.log(await contenedor.getAll())
 }
 
-carrito () ;
+carrito();
